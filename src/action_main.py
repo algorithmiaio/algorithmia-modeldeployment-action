7da24319765b45c3cf79d4dbe879f1cee789de3f
Asli Sabanci <aslisabanci@gmail.com>
2021-02-04 21:45:34 -0800

    Using the GITHUB_WORKSPACE env as-is

--- src/action_main.py
+++ src/action_main.py
@@ -6,9 +6,7 @@ import notebook_executor
 
 
 if __name__ == "__main__":
-    github_workspace = os.getenv("GITHUB_WORKSPACE")
-    # workspace = f"{github_workspace}/main"
-    workspace = github_workspace
+    workspace = os.getenv("GITHUB_WORKSPACE")
     git_repo = os.getenv("GITHUB_REPOSITORY")
     git_ref = os.getenv("GITHUB_REF")
     commit_SHA = os.getenv("GITHUB_SHA")
